13690aab0bb45777b58797fb216e43fe436be320
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-09-05 20:03:22 +0800

    Make cant scroll while modal open

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 
 const Modal = forwardRef(function Modal({ children }, ref) {
   const modalDialog = useRef();
@@ -7,21 +7,38 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     return {
       open() {
         modalDialog.current.showModal();
+        document.body.style.overflow = "hidden";
       },
+
       close() {
         modalDialog.current.close();
+        document.body.style.overflow = "auto";
       },
     };
   });
 
+  const handleClose = (e) => {
+    if (e.target === e.currentTarget) {
+      modalDialog.current.close();
+      document.body.style.overflow = "auto";
+    }
+  };
+
   return (
-    <dialog ref={modalDialog} className="absolute bg-[#D3B17B] p-2 rounded-lg">
+    <dialog
+      ref={modalDialog}
+      onClick={handleClose}
+      className="absolute bg-[#D3B17B] p-2 rounded-lg overflow-hidden"
+    >
       {children}
-      <form method="dialog" className="flex justify-end mt-2">
-        <button className="text-yellow-50 font-semibold bg-[#5F4E33] w-fit p-2 rounded-md">
+      <div className="flex justify-end mt-2">
+        <button
+          onClick={handleClose}
+          className="text-yellow-50 font-semibold bg-[#5F4E33] w-fit p-2 rounded-md"
+        >
           Close
         </button>
-      </form>
+      </div>
     </dialog>
   );
 });
