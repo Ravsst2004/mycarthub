30e4ac5f1c4d7498269133367734ba2a9d4fab2d
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-09-06 10:37:09 +0800

    Change function to use useReducer

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import products from "../data/products.json";
 
 export const CartContext = createContext({
@@ -7,56 +7,74 @@ export const CartContext = createContext({
   handleUpdateItemQuantity: () => {},
 });
 
+const shoopingCartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = products.find((product) => product.id === action.id);
+
+      updatedItems.push({
+        id: action.id,
+        name: product.name,
+        image: product.image,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return { items: updatedItems };
+  }
+
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = state.items
+      .map((item) =>
+        item.id === action.id
+          ? { ...item, quantity: item.quantity + action.amount }
+          : item
+      )
+      .filter((item) => item.quantity > 0);
+
+    return { items: updatedItems };
+  }
+};
+
 export default function CartContextProvider({ children }) {
-  const [shoopingCart, setShoppingCart] = useState({
-    items: [],
-  });
+  const [shoopingCartState, shoopingCartDispatch] = useReducer(
+    shoopingCartReducer,
+    {
+      items: [],
+    }
+  );
 
   const handleAddItemToCart = (id) => {
-    setShoppingCart((prevState) => {
-      const updatedItems = [...prevState.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (item) => item.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = products.find((product) => product.id === id);
-
-        updatedItems.push({
-          id: product.id,
-          name: product.name,
-          image: product.image,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return { items: updatedItems };
+    shoopingCartDispatch({
+      type: "ADD_ITEM",
+      id: id,
     });
   };
 
   const handleUpdateItemQuantity = (id, amount) => {
-    setShoppingCart((prevState) => {
-      const updatedItems = prevState.items
-        .map((item) =>
-          item.id === id ? { ...item, quantity: item.quantity + amount } : item
-        )
-        .filter((item) => item.quantity > 0);
-
-      return { items: updatedItems };
+    shoopingCartDispatch({
+      type: "UPDATE_ITEM",
+      id: id,
+      amount: amount,
     });
   };
 
   const CartValue = {
-    items: shoopingCart.items,
+    items: shoopingCartState.items,
     handleAddItemToCart,
     handleUpdateItemQuantity,
   };
